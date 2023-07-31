ffba641605802d28553aae182ed20460befc122e
mohsin19010 <12mkhan01@gmail.com>
2023-07-31 19:26:42 +0100

    match data done

--- services/riot-ingest/src/test/riot_ingest_integration_test.py
+++ services/riot-ingest/src/test/riot_ingest_integration_test.py
@@ -9,7 +9,7 @@ from protobufs.services import riot_ingest_pb2, riot_ingest_pb2_grpc
 def test_get_match_data() -> None:
     with grpc.insecure_channel(f"riot-ingest:{ os.environ['RIOT_INGEST_SERVICE_PORT' ]}") as channel:
         stub = riot_ingest_pb2_grpc.RiotIngestStub(channel)
-        response = stub.GetMatchData(riot_ingest_pb2.MatchDataRequest(matchId="someId"))
+        response = stub.GetMatchData(riot_ingest_pb2.MatchDataRequest(matchId="id"))
 
     assert response.matchId == "id"
-    assert response.data == "data"
+    assert response.response == "data"
