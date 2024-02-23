dbd88a6589b3940591827cdf5259844856eaccc0
Sami <sami_khalid@hotmail.co.uk>
2024-02-23 01:34:11 +0000

    implemented store match function and test

--- services/match/src/main/service.py
+++ services/match/src/main/service.py
@@ -4,13 +4,16 @@ from concurrent import futures
 
 import grpc
 from config import Config
+from google.protobuf import json_format
 from pymongo import MongoClient
 from service_common.service_logging import init_logging, log_and_flush
 
-from protobufs.services.v1 import match_service_pb2, match_service_pb2_grpc
+from protobufs.services.match.v1 import match_service_pb2, match_service_pb2_grpc
 
 DATABASE_USERNAME_FILE = "/run/secrets/mongo-root-username"
 DATABASE_PASSWORD_FILE = "/run/secrets/mongo-root-password"
+
+MATCH_COLLECTION_NAME = "match"
 
 
 class MatchServicer(match_service_pb2_grpc.MatchService):
@@ -23,9 +26,24 @@ class MatchServicer(match_service_pb2_grpc.MatchService):
             DATABASE_PASSWORD_FILE
         ) as database_password_file:
             self.client = MongoClient(
-                f"mongodb://{database_username_file.read()}:{database_password_file.read()}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
+                f"mongodb://{database_username_file.read()}:{database_password_file.read()}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}?authSource=admin"
             )
-            print(f"db connected to {self.client.get_database().name}")
+            log_and_flush(logging.INFO, f"MongoDB client created...")
+
+    def StoreMatch(
+        self,
+        request: match_service_pb2.StoreMatchRequest,
+        context: grpc.ServicerContext,
+    ) -> match_service_pb2.StoreMatchResponse:
+        result = (
+            self.client[self.DATABASE_NAME]
+            .get_collection(MATCH_COLLECTION_NAME)
+            .insert_one(json_format.MessageToDict(request.match))
+        )
+
+        resp = match_service_pb2.StoreMatchResponse()
+        resp.success = result.acknowledged
+        return resp
 
 
 def serve() -> None:
--- services/match/src/test/integration/match_integration_test.py
+++ services/match/src/test/integration/match_integration_test.py
@@ -2,26 +2,27 @@ import os
 
 import grpc
 
-from protobufs.common.v1 import match_pb2
 from protobufs.services.match.v1 import match_service_pb2, match_service_pb2_grpc
 from protobufs.services.v1 import riot_ingest_pb2, riot_ingest_pb2_grpc
 
 
 def get_riot_match_data():
-    with grpc.insecure_channel(
-        f"riot-ingest:{os.environ['RIOT_INGEST_SERVICE_PORT']}"
-    ) as channel:
+    with grpc.insecure_channel(f"riot-ingest:{os.environ['RIOT_INGEST_SERVICE_PORT']}") as channel:
         stub = riot_ingest_pb2_grpc.RiotIngestServiceStub(channel)
 
         response = stub.GetMatchData(
-            riot_ingest_pb2.GetMatchDataRequest(
-                match_id="7b2412ad-d530-4bec-a112-01b171bb4959"
-            )
+            riot_ingest_pb2.GetMatchDataRequest(match_id="7b2412ad-d530-4bec-a112-01b171bb4959")
         )
 
-        print(str(response))
-    return response
+    return response.match
 
 
 def test_store_match() -> None:
-    print("hello")
+    with grpc.insecure_channel(f"match:{ os.environ['MATCH_SERVICE_PORT']}") as channel:
+        stub = match_service_pb2_grpc.MatchServiceStub(channel)
+
+        riot_match = get_riot_match_data()
+
+        response = stub.StoreMatch(match_service_pb2.StoreMatchRequest(match=riot_match))
+
+        assert response.success is True
