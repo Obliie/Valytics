e7d12c917e0e3356de37a31c6673cff0d2a72bb5
Sami <sami_khalid@hotmail.co.uk>
2024-02-26 01:44:12 +0000

    implemented get match data function and test

--- services/match/src/main/service.py
+++ services/match/src/main/service.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from concurrent import futures
+from typing import Any, Dict
 
 import grpc
 from config import Config
@@ -8,12 +9,16 @@ from google.protobuf import json_format
 from pymongo import MongoClient
 from service_common.service_logging import init_logging, log_and_flush
 
+from protobufs.common.v1 import match_pb2
 from protobufs.services.match.v1 import match_service_pb2, match_service_pb2_grpc
 
 DATABASE_USERNAME_FILE = "/run/secrets/mongo-root-username"
 DATABASE_PASSWORD_FILE = "/run/secrets/mongo-root-password"
 
+PROTOBUF_MATCH_ID_FIELD = "id"
+
 MATCH_COLLECTION_NAME = "match"
+MATCH_ID_FIELD = "matchesInfo.matchId"
 
 
 class MatchServicer(match_service_pb2_grpc.MatchService):
@@ -29,6 +34,29 @@ class MatchServicer(match_service_pb2_grpc.MatchService):
                 f"mongodb://{database_username_file.read()}:{database_password_file.read()}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}?authSource=admin"
             )
             log_and_flush(logging.INFO, f"MongoDB client created...")
+
+    def _match_document_to_match(self, match_document: Dict[Any, Any]) -> match_pb2.Match:
+        match_document.pop("_id")
+        match = match_pb2.Match()
+        json_format.ParseDict(match_document, match)
+
+        return match
+
+    def GetMatchData(
+        self,
+        request: match_service_pb2.GetMatchDataRequest,
+        context: grpc.ServicerContext,
+    ) -> match_service_pb2.GetMatchDataResponse:
+        match_document = (
+            self.client[self.DATABASE_NAME]
+            .get_collection(MATCH_COLLECTION_NAME)
+            .find_one({MATCH_ID_FIELD: request.match_id})
+        )
+
+        resp = match_service_pb2.GetMatchDataResponse()
+        resp.match.CopyFrom(self._match_document_to_match(match_document))
+
+        return resp
 
     def StoreMatch(
         self,
--- services/match/src/test/integration/match_integration_test.py
+++ services/match/src/test/integration/match_integration_test.py
@@ -5,14 +5,14 @@ import grpc
 from protobufs.services.match.v1 import match_service_pb2, match_service_pb2_grpc
 from protobufs.services.v1 import riot_ingest_pb2, riot_ingest_pb2_grpc
 
+test_match_id = "7b2412ad-d530-4bec-a112-01b171bb4959"
+
 
 def get_riot_match_data():
     with grpc.insecure_channel(f"riot-ingest:{os.environ['RIOT_INGEST_SERVICE_PORT']}") as channel:
         stub = riot_ingest_pb2_grpc.RiotIngestServiceStub(channel)
 
-        response = stub.GetMatchData(
-            riot_ingest_pb2.GetMatchDataRequest(match_id="7b2412ad-d530-4bec-a112-01b171bb4959")
-        )
+        response = stub.GetMatchData(riot_ingest_pb2.GetMatchDataRequest(match_id=test_match_id))
 
     return response.match
 
@@ -26,3 +26,12 @@ def test_store_match() -> None:
         response = stub.StoreMatch(match_service_pb2.StoreMatchRequest(match=riot_match))
 
         assert response.success is True
+
+
+def test_get_match_data() -> None:
+    with grpc.insecure_channel(f"match:{ os.environ['MATCH_SERVICE_PORT' ]}") as channel:
+        stub = match_service_pb2_grpc.MatchServiceStub(channel)
+
+        response = stub.GetMatchData(match_service_pb2.GetMatchDataRequest(match_id=test_match_id))
+
+        assert response.match.matches_info.match_id == test_match_id
